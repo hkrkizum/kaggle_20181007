52f4adf10aa977634f2d41fc8aea375d915ac119
Hikaru Koizumi <aoxora333@gmail.com>
2018-10-07 23:02:42 +0900

    no message

--- titanic.R
+++ titanic.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(mice)
+library(caret)
+library(xgboost)
 
-# Get rawdata
+# Get rawdata --------------------------------------------------------------------
 titanic_original <- read_csv("Rawdata/train.csv")
 colnames(titanic_original)
 titanic_original
@@ -38,9 +40,40 @@ rawdata[Sex=="male",Sex:="1"]
 rawdata[Sex=="female",Sex:="2"]
 rawdata$Sex <- as.numeric(rawdata$Sex)
 
+# str Sex to numetric: male 1, female 0
 rawdata[Embarked=="S",Embarked:="1"]
-rawdata[Embarked=="S",Embarked:="1"]
+rawdata[Embarked=="Q",Embarked:="2"]
+rawdata[Embarked=="S",Embarked:="3"]
+
+# Make train data
+n_rows <- nrow(rawdata)
+index <- sample(n_rows, floor(0.8 * nrow(rawdata)))
+rawdata_train <- rawdata[index,] #80%の学習データ
+rawdata_test <- rawdata[-index,] #80%の学習データ
 
 
+#trainControlを設定
+ctrl <- trainControl(method = "cv",   
+                     number = 10,
+                     selectionFunction = "best")
 
+#格子探索用のグリッド
+grid <- expand.grid(nrounds = 500, 
+                    eta = seq(0.01, 0.4, 0.01), 
+                    max_depth = 4:10,
+                    gamma = 0,
+                    colsample_bytree = 1,
+                    min_child_weight = 1,
+                    subsample = 1)
 
+#XGboostでパラメタチューニング
+xgb.tune <- train(Old ~ .,
+                  data = awabi_test,
+                  method = "xgbTree",
+                  metric = "Accuracy",
+                  trControl = ctrl,
+                  tuneGrid = grid)
+
+xgb.tune$bestTune
+nrounds max_depth  eta gamma colsample_bytree min_child_weight subsample
+6     500         9 0.01     0                1                1         1
